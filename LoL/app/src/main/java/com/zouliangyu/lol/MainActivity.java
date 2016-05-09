f83ee8f4284729034eb80df0ad3f71309556b435
邹良禹 <862379727@qq.com>
2016-05-09 14:30:17 +0800

    修改了内容

--- LoL/app/src/main/java/com/zouliangyu/lol/MainActivity.java
+++ LoL/app/src/main/java/com/zouliangyu/lol/MainActivity.java
@@ -3,6 +3,11 @@ package com.zouliangyu.lol;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 邹良禹
+ * LoL掌游宝
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
